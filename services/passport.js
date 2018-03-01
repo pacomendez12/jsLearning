d4836655d0c22fa23f24b1ca9650d11eddd1eab3
Francisco Mendez <pacomendez12@hotmail.com>
2018-02-28 22:17:46 -0600

    sync

--- services/passport.js
+++ services/passport.js
@@ -1,6 +1,19 @@
 const passport = require("passport");
 const GoogleStrategy = require("passport-google-oauth20").Strategy;
+const mongoose = require("mongoose");
 const keys = require("../config/keys");
+
+const User = mongoose.model("users");
+
+passport.serializeUser((user, done) => {
+  done(null, user.id);
+});
+
+passport.deserializeUser((id, done) => {
+  User.findById(id).then(user => {
+    done(null, user);
+  });
+});
 
 passport.use(
   new GoogleStrategy(
@@ -10,9 +23,27 @@ passport.use(
       callbackURL: "/auth/google/callback"
     },
     (accessToken, refreshToken, profile, done) => {
-      console.log("access token", accessToken);
-      console.log("refres token", refreshToken);
+      /*console.log("access token", accessToken);
+      console.log("refres token", refreshToken);*/
       console.log("profile", profile);
+      User.findOne({ googleId: profile.id })
+        .then(existingUser => {
+          if (existingUser) {
+            // User currently exists
+            done(null, existingUser);
+          } else {
+            // New user
+            new User({ googleId: profile.id })
+              .save()
+              .then(user => done(null, user))
+              .catch(error => {
+                assert.isNotOk(error, "Promise error");
+              });
+          }
+        })
+        .catch(error => {
+          assert.isNotOk(error, "Promise error");
+        });
     }
   )
 );
